cfabd6bb20b177d8e523f70e431f39e4f3915410
opilane <opilane@HP-CZC2349HTR>
2024-03-21 15:46:17 +0200

    21.03.2024

--- PythonApplication1/registracija.py
+++ PythonApplication1/registracija.py
@@ -1,13 +1,48 @@
-from registracija_module import * 
-kasutajad=[]
-parolid=[]
-da=0
-while da==0:
-    vibor1=int(input(" 1-log in \n 2-sign in \n 3-info\n"))
-    if vibor1==1:
-        log_in(kasutajad,parolid,da)
-    elif vibor1==2:
-        sign_in(kasutajad,parolid)
-    elif vibor1==3:
-        info(kasutajad,parolid,da)
-print("uraaaaaaa!!!")
+﻿import smtplib
+import ssl
+from email.message import EmailMessage
+from registracija_module import *
+
+kasutajad = []
+valid = []
+paroolid = []
+read_file("registreerimine.txt", kasutajad, paroolid, valid)
+while True:
+    print("Valikud \n 1.Registratsija\n 2.Avtorizatsija\n 3.Smena parola\n 4.ja zabil parol \n 5.Spisok polzovatelei \n 6.Zakonchit \n 7. Otpravka pisma")
+    valik = input("Vali tegevus: ")
+    if valik == "1":  # Регистрация
+        registreerimine(kasutajad, paroolid, valid)
+    elif valik == "2":  # Авторизация
+        nimi = input("Vvedite username: ")
+        parool = input("Vvedite parol: ")
+        if autoriseeri_kasutaja(nimi, parool, kasutajad, paroolid):
+            print("Vi avtorizovalis")
+            print()
+        else:
+            print("Vi ne avtorizovalis")
+            print()
+    elif valik == "3":  # Изменить пароль
+        nimi = input("Vvedite username: ")
+        vana_parool = input("Vvedite stari parol: ")
+        uus_parool = input("Vvedite novi parol: ")
+        print(smena_parola(nimi, vana_parool, uus_parool, kasutajad, paroolid))
+    elif valik == "4":  # Забыли пароль
+        nimi = input("Vvedite username: ")
+        print(zabil_parol(nimi, kasutajad, paroolid))
+    elif valik == "5":  # Просмотр добавленных пользователей
+        vivod_polzovatelej(kasutajad, paroolid, valid)
+    elif valik == "6":  # Выход
+        print("programma zavershilas")
+        print()
+        break
+    elif valik == "7":
+        K = input("Vvedite svoi email: ")
+        index1 = None
+        for index, L in enumerate(valid):
+            if L == K:
+                index1 = index
+                break
+        prislat_parol (K,paroolid[index1])
+    else:
+        print("Neverni vibor")
+        print()
--- PythonApplication1/registracija_module.py
+++ PythonApplication1/registracija_module.py
@@ -1,26 +1,98 @@
-def info(k:list,p:list,da:int)->any:
-    """
+﻿import random
+import string
+import smtplib, ssl
 
-    """
-    print("kasutajad: ", k)
-    print("parolid: ", p)
-    print(da)
+def vivod_polzovatelej(kasutajad, paroolid, mail):
+    for i in range(len(kasutajad)):
+        print(f"{kasutajad[i]}, {mail[i]}, {paroolid[i]}")
+
+def registreerimine(kasutajad, paroolid, mail):
+    nimi = input("Vvedite username: ")
+    if nimi not in kasutajad:
+        email=input("Sisesta email: ")
+        parool_valik = input("Sozdat svoi parol (vedite 'ja') ili sgenerirobat random (vvedite 'ei')? ")
+        if parool_valik.lower() == "ja":
+            parool = input("vvedite parol: ")
+            paroolid.append(parool)
+            mail.append(email)
+            kasutajad.append(nimi)
+            save(nimi, parool, email, "registreerimine.txt")
+            print("Vi zaregistrirovalis!")
+        else:
+            parool = random_parol()
+            paroolid.append(parool)
+            mail.append(email)
+            kasutajad.append(nimi)
+            save(nimi, parool, email, "registreerimine.txt")
+            print("Vi zaregistrirovalis!")
+            print("Novi parol: ", parool)
+
+def autoriseeri_kasutaja(nimi, parool, kasutajad, paroolid):
+    if nimi in kasutajad:
+        if paroolid[kasutajad.index(nimi)] == parool:
+            return True
+    return False
+
+def smena_parola(nimi, vana_parool, uus_parool, kasutajad, paroolid):
+    if autoriseeri_kasutaja(nimi, vana_parool, kasutajad, paroolid):
+        paroolid[kasutajad.index(nimi)] = uus_parool
+        return "Parol izmenen"
+    return "Oshibka, proverte dannie"
+
+def zabil_parol(nimi, kasutajad, paroolid):
+    if nimi in kasutajad:
+        indeks = kasutajad.index(nimi)
+        uus_parool = random_parol()
+        paroolid[indeks] = uus_parool 
+        print("Sozdan novi parol: " + uus_parool)
+    print("Polzovatela ne naiden")
+
+def random_parol(pikkus=8):
+    parool = ''.join(random.choices(string.ascii_letters + string.digits, k=pikkus))
+    return parool
+
+def save(uus_user, uus_parool, uus_email, registreerimine):
+    save =f"{uus_user}_{uus_parool}:{uus_email}"
+    with open (registreerimine, "a") as s:
+        s.write(f"{save}\n")
+
+def read_file (file, kasutajad, paroolid, mail):
+    with open(file, 'r') as s:
+        data = s.readlines()
+        if len(data):
+            for line in data:
+                name_password_and_email = line.strip().split(':')
+                email = name_password_and_email[1]
+                name_and_password = name_password_and_email[0].split("_")
+                name = name_and_password[0]
+                password = name_and_password[1]
+                kasutajad.append(name)
+                paroolid.append(password)
+                mail.append(email)
+
+def prislat_parol(to_email, parool):
+    from email.message import EmailMessage
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    sender_email = "erik.gerega@gmail.com"
+    password = "baqd vgeh msad xbbm "
+    context = ssl.create_default_context()
+    msg = EmailMessage()
+    msg.set_content(f"vas parol - {parool}")
+    msg['Subject'] = "vas parol!"
+    msg['From'] = sender_email
+    msg['To'] = to_email
 
 
-def log_in(k:list,p:list,da:int)->any:
-    """
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()
+        server.starttls(context=context)
+        server.ehlo()
+        server.login(sender_email, password)
+        server.send_message(msg)
 
-    """
-    username=input("Vvedite username: ")
-    parol=input("Vvedite parol: ")
-    k.append(username)
-    p.append(parol)
-    da=1
-    return k,p,da
-
-
-
-def sign_in():
-    """
-
-    """
+    except Exception as e:
+        print(e)
+    finally:
+        server.quit()
